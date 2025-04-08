7c6c920af4fb2bebcaee42d00cee3959bf154579
杨再涛 <1772081518@qq.com>
2025-04-09 02:23:35 +0800

    调整立体阴影效果

--- src/renderer/src/components/CardImg.tsx
+++ src/renderer/src/components/CardImg.tsx
@@ -11,14 +11,22 @@ export default function CardImg({ url, children }: CardProps): JSX.Element {
     width: '100%',
     objectFit: 'cover',
     borderRadius: '8px',
-    boxShadow: '0 4px 8px rgba(0, 0, 0, 0.2)',
+    boxShadow: 'var(--border-show)',
     transition: 'transform 0.2s'
   }
+  const labelStyle: React.CSSProperties = {
+    textShadow: 'var(--border-show)'
+  }
+
   return (
     <div className="card_img">
       <img style={imgStyle} src={url} alt="" />
-      <div className="title">我们的约定</div>
-      <p className="disc">导演、小杨、李妹</p>
+      <div className="title" style={labelStyle}>
+        我们的约定
+      </div>
+      <p className="disc" style={labelStyle}>
+        导演、小杨、李妹
+      </p>
       {children}
     </div>
   )
