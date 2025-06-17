3221d98910c4450a2ed878f999b4513421e9cacc
杨再涛 <1772081518@qq.com>
2025-06-17 16:08:31 +0800

    优化弹窗提示明确性

--- src/main/core/yerror_event.ts
+++ src/main/core/yerror_event.ts
@@ -2,15 +2,20 @@ import { BaseWindow, dialog } from "electron";
 
 /**处理process错误 */
 export class YErrorEvent {
-    constructor(win: BaseWindow) {
-        process.on('uncaughtException', (error) => {
-            dialog.showMessageBox(win, {
-                title: '进程提示',
-                message: error.message
-            })
-        });
+    constructor(private _win: BaseWindow) {
+        process.on('uncaughtException', (error) => this.showDialog(error));
         process.on('unhandledRejection', (reason) => {
             console.error('未处理的 Promise 拒绝:', reason);
         });
+    }
+    showDialog(error: any) {
+        setTimeout(() => {
+            dialog.showMessageBox({
+                type: 'warning',
+                title: '好看韩剧3',
+                message: error.message,
+                detail: '若数据加载不出来,请尝试检查网络重启程序',
+            })
+        }, 1500);
     }
 }
